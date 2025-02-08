bbc6822d6a995b9d52e067306ee3bd30e6dd3000
Sumit Arjun <76439411+sumitsbedre@users.noreply.github.com>
2025-02-08 19:45:03 +0530

    Dimension_cheker.py

--- project/dimension_cheker.py
+++ project/dimension_cheker.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import load_model
 
-model_path = "D:\sem\IBM experments\project\gender_classification_model.h5"  # Replace with your model path
+model_path = "project\gender_classification_model.h5"  # Replace with your model path
 model = load_model(model_path)
 
 # Check the input shape
